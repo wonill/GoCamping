0d817d18b8b8c1bdad958bd884517686e9e4df87
wonil <nwi6959@gmail.com>
2024-08-21 16:41:18 +0900

    캠핑장 연락처 추가

--- src/js/data.js
+++ src/js/data.js
@@ -180,7 +180,7 @@ const createList = (campingSite) => {
               </div>
               <div>
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiPebaQOzJl5_rzH_c5EAaygIuJdtp7n4I9Q3NowGU7jQF-ahw51tagfIJHb68un0xOJk&usqp=CAU" alt="연락처" />
-                <span class="tel">연락처 없음</span>
+                <span class="tel">${campingSite.tel || "연락처 없음"}</span>
                 <a href="#" class="details" data-id="${
                   campingSite.contentId
                 }">상세보기</a>
